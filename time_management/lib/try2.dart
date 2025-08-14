07ecfba362dc6b2f3f599c481b6590d78eb4d0fa
zannatul-prome <zannatulprome20@gmail.com>
2025-08-14 16:55:23 +0600

    3rd part done-----2 box create

--- time_management/lib/try2.dart
+++ time_management/lib/try2.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home:Scaffold(
-      appBar: AppBar(
-        title: Text("Task Manager",style: TextStyle(color: const Color.fromARGB(255, 0, 8, 12),fontSize: 32.0,fontWeight: FontWeight.bold),),
-        centerTitle: true,
-        flexibleSpace: Container(decoration: BoxDecoration(gradient: LinearGradient(colors: [const Color.fromARGB(255, 6, 169, 233),const Color.fromARGB(255, 217, 221, 233)])),),
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Welcome to Task Manager",
+            style: TextStyle(
+              color: const Color.fromARGB(255, 26, 43, 202),
+              fontSize: 32.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: true,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  const Color.fromARGB(255, 6, 169, 233),
+                  const Color.fromARGB(255, 217, 221, 233),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
-    )
-  ));
+    ),
+  );
 }
+
